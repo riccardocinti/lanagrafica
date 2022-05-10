a19201338d20a6374c2fcd97684682e601a42931
Riccardo <cintiriccardo@gmail.com>
2022-05-10 21:32:45 +0200

    AppError implementation

--- src/errors.rs
+++ src/errors.rs
@@ -1,4 +1,6 @@
+use actix_web::{error, http::StatusCode, HttpResponse, Result};
 use serde::Serialize;
+use std::fmt;
 
 #[derive(Debug, Serialize)]
 pub enum AppError {
@@ -9,4 +11,46 @@ pub enum AppError {
 #[derive(Debug, Serialize)]
 pub struct AppErrorResponse {
   error_msg: String,
+}
+
+impl AppError {
+  fn error_response(&self) -> String {
+    match self {
+      AppError::ActixError(msg) => {
+        println!("Server error occurred: {:?}", msg);
+        "Internal server error".into()
+      }
+      AppError::NotFound(msg) => {
+        println!("Not found error occurred: {:?}", msg);
+        msg.into()
+      }
+    }
+  }
+}
+
+impl error::ResponseError for AppError {
+  fn status_code(&self) -> StatusCode {
+    match self {
+      AppError::ActixError(msg) => StatusCode::INTERNAL_SERVER_ERROR,
+      AppError::NotFound(msg) => StatusCode::NOT_FOUND,
+    }
+  }
+
+  fn error_response(&self) -> HttpResponse {
+    HttpResponse::build(self.status_code()).json(AppErrorResponse {
+      error_message: self.error_response(),
+    })
+  }
+}
+
+impl fmt::Display for EzyTutorError {
+  fn fmt(&self, f: &mut fmt::Formatter) -> Result<(), fmt::Error> {
+    write!(f, "{}", self)
+  }
+}
+
+impl From<actix_web::error::Error> for EzyTutorError {
+  fn from(err: actix_web::error::Error) -> Self {
+    EzyTutorError::ActixError(err.to_string())
+  }
 }
